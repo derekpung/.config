f673808b90bff08f47c0d6255578faa991dd4c5a
Derek Pung <derekpung@outlook.com>
2023-07-13 20:56:58 +0800

    i3 and script fix

--- .local/scripts/scratchnotes.sh
+++ .local/scripts/scratchnotes.sh
@@ -4,10 +4,15 @@ nowstr=$(date +"%Y%m%dT%H%M%S")
 selected=~/work/scratchnotes
 selected_name=scratchnotes
 tmux_running=$(pgrep tmux)
+filepath=${selected}/scratch_${nowstr}.md
 
-nviminit() {
-    filepath=${selected}/scratch_${nowstr}.md
-    nvim $filepath
-}
-nviminit
+if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+    tmux new-session -s $selected_name -c $selected -d
+fi
 
+if ! tmux has-session -t=$selected_name 2> /dev/null; then
+    tmux new-session -ds $selected_name -c $selected -d
+fi
+
+tmux send-keys -t $selected_name:1 'nvim ' $filepath Enter
+tmux attach-session -t $selected_name
